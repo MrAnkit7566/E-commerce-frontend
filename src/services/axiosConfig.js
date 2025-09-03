03a57eed3ab7d5fca4b4bffc58e84fba77e877cc
Ankit Kushwah <ankitdeepu910@gmail.com>
2025-09-03 11:03:03 +0530

    changes

--- src/services/axiosConfig.js
+++ src/services/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "https://e-commerce-backend-chi-umber.vercel.app", // ✅ https:// add kiya
+  baseURL: "https://e-commerce-backend-8p5r.onrender.com", // ✅ https:// add kiya
   headers: {
     "Content-Type": "application/json",
   },
